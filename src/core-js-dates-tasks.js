5dff52084f9bccc9389ae1256155784bbc0f65bd
vladimirovicp <vladimirovicp70@gmail.com>
2024-03-22 16:50:30 +0400

    8 pending

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -87,8 +87,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const dateOjb = new Date(year, month, 0);
+  return dateOjb.getDate();
 }
 
 /**
@@ -102,8 +103,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const diff = Date.parse(dateEnd) - Date.parse(dateStart);
+  const numberMilSecDay = 1000 * 60 * 60 * 24;
+
+  return 1 + diff / numberMilSecDay;
 }
 
 /**

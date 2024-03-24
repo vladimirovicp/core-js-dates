eeb7f40cf267637fb61361d9d36d34c03a78efab
vladimirovicp <vladimirovicp70@gmail.com>
2024-03-24 19:24:09 +0400

    pending 3

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -215,8 +215,16 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  if (date.getDay() === 5 && date.getDate() === 13) {
+    return date;
+  }
+  date.setDate(13);
+  while (date.getDay() !== 5) {
+    date.setMonth(date.getMonth() + 1);
+  }
+
+  return date;
 }
 
 /**
